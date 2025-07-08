1e941d9a8c69a8aecf7eb32df71de94fadd6a053
Arshitha-20 <124654516+Arshitha-20@users.noreply.github.com>
2025-07-08 15:48:17 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // ✅ ADD THIS LINE
- app.enableCors({
-  origin: 'https://jobboard-frontend-o1lc.onrender.com/',
+  app.enableCors({
+  origin: ['https://jobboard-frontend-o1lc.onrender.com'],
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  credentials: true,
 });
-
-  await app.listen(3001);
 }
 bootstrap();
